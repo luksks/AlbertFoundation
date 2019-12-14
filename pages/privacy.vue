455348b0ff2ffd148d515bc3375f8e748061022a
AlbertLanne <albert.lanne@gmail.com>
2019-12-14 14:12:41 +0100

    new update test

---
+++ pages/privacy.vue
@@ -0,0 +1,132 @@
+<template>
+
+    <div class=" mx-16 ">
+        <div class="items-center">
+            <h1 class="font-normal text-2xl py-4">
+                INTRODUCTION
+            </h1>
+
+            <p>The data controller pursuant to the General Data Protection Directive (“GDPR”) in respect of any Personal
+                Information that you submit to us via my website
+                Personal Information you submit to us via the AlbertFoundation website (or otherwise) will be processed
+                by us or by data processors appointed by us to undertake processing on our behalf. AlbertFoundation
+                respects your privacy rights and recognizes the importance of protecting the information collected
+                about you. We have adopted this Privacy Policy to explain how we collect, use and store your personal
+                and non-personal information as set forth in the GDPR. If you want to know what Personal Information or
+                Non-Personal Information we hold about you or if you have any other queries in relation to this Privacy
+                Policy, please contact us by emailing us at albert.lanne@gmail.com .
+                We will require you to verify your identity before releasing any information to you.</p>
+
+
+            <h2 class="font-normal text-2xl py-4">
+                2. What Is Personal And Non-Personal Information?
+            </h2>
+            “Personal Information” is information that we have collected from you that identifies you, or which, in
+            conjunction with other information that is in our possession, or is likely to come into our possession, may
+            be used by us to identify you.
+
+            “Non-Personal Information”, is information that we have collected from you which cannot be used by us to
+            identify you.
+            <h3 class="font-normal text-2xl py-4">
+                3. Which Data Does AlbertFoundation Collect And Use At Which Stage?
+            </h3>
+            <h4 class="font-normal text-2xl py-4">
+                3.1. Use Of Our Website
+            </h4>
+            <h5 class="font-normal text-1xl py-4">
+                3.1.1. General
+            </h5>
+
+
+            If you access or use our Website we may collect the following Non-Personal Information:
+
+            Request (Name of the requested file)
+            Browser type/version (e.g.: Internet Explorer 11.0)
+            Browser language (e.g.: English)
+            Operating System (e.g.: Windows 7)
+            Resolution of the Browser window
+            Screen resolution
+            Javascript active
+            Java On /Off
+            Cookies On / Off
+            Color settings
+            Referral URL (the previously visited page)
+            Time of Access
+            Clicks
+
+            <h6 class="font-normal text-1xl py-4">
+                3.1.2. Cookies
+            </h6>
+
+
+            We use cookies to ensure that you get the most out of my website Services. Cookies are small text files that
+            are applied to your Internet enabled device by websites. Cookies allow us to store and then retrieve
+            information on your computer about your visit to our Website (e.g. when you accessed the Website). We may
+            use cookies to deliver content specific to your interest and to monitor Website usage or to simplify your
+            visits to the website Services(for example, by remembering your data details).
+
+            We may also store flash cookies, also known as “local shared objects,” on your Internet enabled device if
+            the Website uses Adobe Flash. Flash cookies are small files similar to cookies and are used to remember the
+            Website’s settings to personalize the look and feel of the Website. Flash cookies only collect data in the
+            aggregate. Like normal cookies, Flash cookies are represented as small text files on your internet enabled
+            device.
+
+            The collection of information is not mandatory. Most browsers are automatically set to
+            accept cookies whenever you visit a website. You can disable cookies or set your browser to alert you when
+            cookies are being sent. However, your web experience may be less satisfying without the use of cookies. For
+            more information about how to reject cookies using your internet browser settings please consult the “Help”
+            section of your internet browser (or alternatively visit http://www.aboutcookies.org).
+
+            The first time you visit the Website you will be presented with a notification that we use cookies. By
+            proceeding to use the Website you are consenting to our use of cookies as described in this Privacy Policy.
+            <p class="font-normal text-1xl py-4">
+                3.1.3. Analytic Metrics Tools And Other Technologies
+            </p>
+            <p>
+                AlbertFoundation may also use its own or third-party proprietary analytic metrics tools and other
+                analytics
+                technologies
+                to
+                collect the information referred to in this Privacy Policy. We may use such technology to deliver
+                content
+                specific
+                to your interest and to monitor usage of the AlbertFoundation Services.
+
+                Our website uses Google Analytics. To learn how Google uses your data and how you can opt out click
+                here:
+                https://www.google.com/policies/privacy/partners/. The collection of the information set forth in this
+                Section
+                3.1.3 is not mandatory.
+            </p>
+
+            <p class="font-normal text-1xl py-4">
+                3.6 Marketing Information
+            </p>
+            <p class="mb-8">
+                Subject to your approval we may also use the information collected under Section 3.1 to 3.3 to provide
+                marketing
+                information and special offers for the Game to you. This use of the information collected under Sections
+                3.1
+                to
+                3.3 is
+                not mandatory. You can opt-out of this use of your data at any time here.
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "analytics",
+        head() {
+            return {
+                title: "analytics page"
+            };
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>
