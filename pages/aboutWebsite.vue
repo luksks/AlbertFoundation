e471c1186b6810426673e24b30d37c013f9e6e28
AlbertLanne <albert.lanne@gmail.com>
2019-12-17 21:10:08 +0100

    update

--- pages/aboutWebsite.vue
+++ pages/aboutWebsite.vue
@@ -1,6 +1,15 @@
 <template>
-<div>
+<div class="bg-background-primary text-copy-primary">
     <h1>The structure of the website</h1>
+
+
+    <p>Nuxt.js, vue.js</p>
+    <p>node.js, javascript</p>
+    <p>TailwindCSS</p>
+
+    <p>Clever Cloud</p>
+    <p>OVH</p>
+    <p>Google Analytics</p>
 <div class="h-full w-full ">
 
 
