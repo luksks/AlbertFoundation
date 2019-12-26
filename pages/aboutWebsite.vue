aead9d3ce85821317cf996601670ff162730f3ce
AlbertLanne <albert.lanne@gmail.com>
2019-12-26 15:13:29 +0100

    Update of the About page, and timeline created

--- pages/aboutWebsite.vue
+++ pages/aboutWebsite.vue
@@ -1,65 +1,96 @@
 <template>
+    <div>
+
+        <div class="mt-4 h-full w-full flex items text-copy-primary bg-background-primary">
 
 
-            <div class="h-full w-full text-copy-primary bg-background-primary">
-                <div class="mx-8 m-8  flex-wrap bg-background-secondary align-baseline items-center  hover:shadow-xl px-3 py-2 m-2 rounded ">
-                    <p class="text-3xl my-4">The structure of the website</p>
+            <div class=" mx-4 my-4  flex-wrap bg-background-secondary align-baseline items-center  hover:shadow-xl px-3 py-2 m-2 rounded ">
+                <p class="text-3xl my-4">The structure of the website</p>
 
-                    <div class="mx-4 ">
-
-                        <div class="align-baseline flex text-2xl">
-                            <div class=" rounded-lg justify-start w-full">
-                                <div class="justify-end items-end flex">
-                                    <p>This website is made with the javascript framework NuxtJS and Vue.JS and cooked
-                                        by me
-                                        with love. Nuxt.js is
-                                        a great french framework for the server-side rendering! I use too webpack and
-                                        node.js
-                                        for the environment of development and compilation. For the front end I use. </p>
-                                </div>
-                            </div>
-
-                            <div class="sm:mx-24 mx-6 justify-end flex">
-                                <picture>
-                                    <img class="rounded-full bg-white border-solid  shadow-insethover:border-green-600 "
-                                         height="160"
-                                         width="160"
-                                         alt="My favorite french js framework ♥"
-                                         src="../assets/img/AboutWebsite/nuxt.png">
-                                </picture>
+                <div class="mx-4 ">
+                    <div class="align-baseline flex text-2xl">
+                        <div class=" rounded-lg justify-start w-full">
+                            <div class="justify-end items-end flex">
+                                <p>This website is made with the javascript framework NuxtJS and Vue.JS and cooked
+                                    by me
+                                    with love. Nuxt.js is
+                                    a great french framework for the server-side rendering! I use too webpack and
+                                    node.js
+                                    for the environment of development and compilation. For the front end I use. </p>
                             </div>
                         </div>
 
-                        <div class="flex">
+                        <div class="sm:mx-24 mx-6 justify-end flex">
+                            <picture>
+                                <img class="rounded-full bg-white border-solid "
+                                     height="260"
+                                     width="260"
+                                     alt="My favorite french js framework ♥"
+                                     src="../assets/img/AboutWebsite/nuxt.png">
+                            </picture>
                         </div>
-                        <div class="align-baseline  flex">
-                            <div class="sm:mx-24 mx-6 justify-start flex">
-                                <picture>
-                                    <img
-                                            class="border-solid rounded-full shadow-inset hover:border-green-600"
-                                            height="160"
-                                            width="160"
-                                            alt="The best Paas ever"
-                                            src="../assets/img/AboutWebsite/logo_clever_cloud.jpg">
-                                </picture>
-                            </div>
-                            <div class="text-2xl rounded-lg justify-end w-full ">
-                                <p>For the hosting i use a French PAAS, Clever Cloud. Usefull for a easier deployment
-                                    and a htpps certificat. the support help a lot. For the hosting i use OVH. </p>
-                            </div>
+                    </div>
+
+                    <div class="align-baseline  my-6 flex">
+                        <div class="sm:mx-24 mx-6 justify-start flex">
+                            <picture>
+                                <img
+                                        class="border-solid rounded-full shadow-inset hover:border-green-600"
+                                        height="160"
+                                        width="160"
+                                        alt="The best Paas ever"
+                                        src="../assets/img/AboutWebsite/logo_clever_cloud.jpg">
+                            </picture>
+                        </div>
+                        <div class="text-2xl rounded-lg justify-end w-full ">
+                            <p>For the hosting i use a French PAAS, Clever Cloud. Usefull for a easier deployment
+                                and a htpps certificat. the support help a lot. For the hosting i use OVH. </p>
                         </div>
                     </div>
                 </div>
-                <div class="hover:shadow-xl mx-8 m-8  flex-wrap bg-background-secondary align-baseline items-center hover:shadow-xl px-3 py-2 m-2 rounded ">
-                    <p class="text-3xl my-4">Data science </p>
+            </div>
+            <div class=" container mx-4  my-4 bg-background-secondary align-baseline items-center hover:shadow-xl px-3 py-2 m-2 rounded ">
+                <p class="text-3xl my-4">Data science</p>
 
-                    <p class="text-2xl">I use different metriques tools for analyse the data of my websites and improve it
-                        are google Analytics, clever cloud information, Google search Engine. You can see here more
-                        information about my <nuxt-link to="/privacy">privacy policy here.</nuxt-link></p>
-<!--                    <p class="text-3xl my-4">Google Ads</p>-->
-                </div>
+                <p class="text-2xl text-justify flexflex-wrap ">The website has an important backend of data analysis. I
+                    spend an important
+                    time to watch the video of google SkillShop and have the certification on google analytics, google
+                    ads
+                    and general e-marketing. For the websites, I use different metrics tools for analyse the data of it
+                    and
+                    improve the experiences of the users. Mostly I watch the data from CleverCloud, Google Search
+                    Console
+                    and Google Analytics. You can see here more information about my
+                    <nuxt-link to="/privacy">privacy policy here.</nuxt-link>
+                </p>
+                <!--                    <p class="text-3xl my-4">Google Ads</p>-->
+            </div>
         </div>
 
+        <div class="mb-8 text-copy-primary hover:shadow-xl mt-4 mx-4 bg-background-secondary
+                      items-baseline flex hover:shadow-xl px-3 py-2 m-2 rounded ">
+
+            <div class="justify-start flex flex-wrap">
+                <p class="text-3xl my-4">Data science certification</p>
+                <p class="text-2xl ">To understand and use well the data, I need to have certification. I have the
+                    Google
+                    certification of the Fundamental principles of digital marketing. And actually I'm working on more
+                    advanced
+                    google ads and google analytics certification.
+                </p>
+            </div>
+
+            <div class="justify-end flex flex-wrap">
+                <picture class="flex w-2/4">
+                    <img class="rounded-lg "
+                         alt="My google certificat"
+                         src="../assets/img/AboutWebsite/certificat_de_base.png">
+                </picture>
+            </div>
+
+            <!--                    <p class="text-3xl my-4">Google Ads</p>-->
+        </div>
+    </div>
 </template>
 
 <script>
