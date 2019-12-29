5b280d20fab6712712a6b0bfa411abe101487f5b
AlbertLanne <albert.lanne@gmail.com>
2019-12-29 16:13:17 +0100

    responsives design for mobile

--- components/NavFooter.vue
+++ components/NavFooter.vue
@@ -1,8 +1,10 @@
 <template>
     <footer class=" bg-background-tertiary flex">
-        <div class=" items-center flex flex-col sm:flex-row container mx-auto text-copy-primary flex-wrap">
+        <div class="items-center flex flex-col sm:flex-row container mx-auto text-copy-primary flex-wrap">
 
-            <div class="sm:hidden lg:block m-3 mx-12 flex justify-center">
+            <div class="xl:w-1/4 sm:w-2/4 w-full sm:justify-start justify-center
+                            lg:block m-3 mx-auto flex ">
+
                 <nuxt-link to="/" class="sm:text-2xl text-3xl flex text-center">
                     Albertfoundation 📡
                 </nuxt-link>
@@ -14,12 +16,14 @@
                 <!--                            class="right"/></a></p>-->
             </div>
 
-            <div class="w-1/4 my-3 sm:text-left text-center">
+            <div class="sm:w-2/4 xl:w-1/4 w-full my-3 sm:text-left text-center">
                 <p class=" mb-4 hover:text-green-600 sm:text-xl text-2xl ">Ressources</p>
                 <!--                        zoom text-center  no-underline shadow-lg  border border-border-color-primary  px-3 py-2 m-2 rounded -->
-                <a class="sm:p-0 sm:px-0 p-1 px-3 sm:border-0 border hover:shadow-xl border-border-color-primary rounded my-2 hover:text-blue-700"
+                <a class="flex sm:justify-start justify-center sm:mb-0 mb-4
+                 sm:p-0 sm:px-0 p-1 px-2 sm:border-0 border hover:shadow-xl border-border-color-primary rounded my-2 hover:text-blue-700"
                    download
                    href="CV_LANNE_Albert.pdf">- CV</a>
+
                 <nuxt-link to="/presentation">
                     <p class="sm:p-0 sm:px-0 p-1 px-2 sm:border-0 border hover:shadow-xl border-border-color-primary rounded my-2 hover:text-blue-700">
                         - Presentation</p>
@@ -30,11 +34,12 @@
                 <!--          <p class="cursor-pointer hover:text-red-900 my-2 my-2">- Incoming</p>-->
             </div>
 
-            <div class="w-1/4 my-3 ">
-                <p class="mb-4 hover:text-green-600 sm:text-xl text-2xl  ">More</p>
+            <div class="sm:w-2/4 xl:w-1/4 w-full my-3 sm:text-left text-center">
+                <p class=" mb-4 hover:text-green-600 sm:text-xl text-2xl ">More</p>
 
-                <a class="sm:text-left text-center cursor-pointer sm:p-0 sm:px-0 p-1 px-3 sm:border-0 border hover:shadow-xl border-border-color-primary rounded my-2 hover:text-blue-700">
-                    Theme Dark
+                <a class="flex sm:justify-start justify-center sm:mb-0 mb-4
+                 sm:p-0 sm:px-0 p-1 px-2 sm:border-0 border hover:shadow-xl border-border-color-primary rounded my-2 hover:text-blue-700">
+                    - Theme Dark
                 </a>
                 <p class="sm:text-left text-center cursor-pointer sm:p-0 sm:px-0 p-1 px-3 sm:border-0 border hover:shadow-xl border-border-color-primary rounded my-2 hover:text-blue-700"
                    title="The opensource repo of the website !"><a
@@ -42,11 +47,13 @@
                         target="_blank">- GitHub repo</a></p>
                 <!--          <p class="cursor-pointer hover:text-red-900 my-2">- Incoming</p>-->
             </div>
-            <div class="w-1/4 my-3">
-                <p class="mb-4 hover:text-green-600 sm:text-xl text-2xl ">Contact</p>
+
+            <div class="sm:w-2/4 xl:w-1/4 w-full my-3 sm:text-left text-center">
+                <p class=" mb-4 hover:text-green-600 sm:text-xl text-2xl ">Contact</p>
 
                 <nuxt-link to="/privacy">
-                    <p class="sm:text-left text-center sm:p-0 sm:px-0 p-1 px-2 sm:border-0 border border-border-color-primary rounded my-2 hover:text-blue-700">
+                    <p class="sm:mb-0 mb-4
+                                sm:text-left text-center sm:p-0 sm:px-0 p-1 px-2 sm:border-0 border border-border-color-primary rounded my-2 hover:text-blue-700">
                         - Privacy policy</p>
                 </nuxt-link>
                 <nuxt-link to="/aboutWebsite">
