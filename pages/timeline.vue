fb5053c11ee1d5f23f4be611337ea86c651f029b
AlbertLanne <albert.lanne@gmail.com>
2020-02-03 09:59:01 +0100

    update timeline

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -105,10 +105,10 @@
             <div class="bg-grey-lighter p-4 pl-8 text-copy-primary" v-show="!isNinja">
                 <textarea class="px-2 w-full border border-blue rounded hover:bg-background-primary bg-background-tertiary italic font-mono  focus:outline-none focus:shadow-outline"></textarea>
                 <span class="flex justify-between items-center mt-2">
-                    <button class="p-2 text-blue leading-none rounded border border-transparent hover:border-blue">
+                    <button class=" text-blue leading-none rounded border border-transparent hover:border-blue">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                             class="feather feather-image stroke-current text-copy-primary ">
+                             class="feather feather-image stroke-current text-copy-primary focus:outline-none focus:shadow-outline">
                             <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
                             <circle cx="8.5" cy="8.5" r="1.5"></circle>
                             <polyline points="21 15 16 10 5 21"></polyline>
