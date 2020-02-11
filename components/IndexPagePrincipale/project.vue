80325c9cb1fe392a266273abc2be10f444762862
AlbertLanne <albert.lanne@gmail.com>
2020-02-11 10:21:03 +0100

    add new project on index and about website update

--- components/IndexPagePrincipale/project.vue
+++ components/IndexPagePrincipale/project.vue
@@ -1,28 +1,25 @@
 <template>
 
-    <div class="w-full flex  items-center py-16 flex-grow">
-
+<div class="w-full flex  items-center py-16 flex-grow">
 <div class="mx-8 sm:align-baseline  align-middle items-center sm:flex w-full">
 
-            <div class="sm:w-1/3 w-full sm:mt-0 mt-16
-                        px-4 h-full items-stretch justify-center flex">
-                <div class="h-full  w-full shadow-3xl autre mx-auto justify-center items-center rounded-lg -p-4 hover:bg-black border border-gray-800">
-                    <a href="https://github.com/AlbertLanne/ThemeJS_TailwindCSS_NuxtJS">
-                        <img src="../../assets/img/index/Octocat.png" alt="project" class="my-6 h-32 w-auto mx-auto"> </a>
-                    <div class="w-full flex">
-                        <p class="text-white text-xl text-center flex mx-auto font-mono">Theme switcher Tailwindcss project</p>
-                    </div>
-                </div>
-            </div>
+    <div class="sm:w-1/3 w-full sm:mt-0 mt-16 relative px-4 h-full items-stretch justify-center flex">
+        <div class="h-full  w-full shadow-3xl autre mx-auto justify-center items-center rounded-lg -p-4 hover:bg-black border border-gray-800">
+            <img src="https://jdmmedical.fr/img/specialiste-handicap-maintien-domicile-jdm-medical-xl.jpg" alt="project" class="h-48 w-full bg-right-bottom flex object-fill bg-cover">
+        </div>
+        <div class= "absolute">
 
+            <p class="text-white text-xl text-center flex mx-auto font-mono mt-2">JDM Médical</p>
+            <a href="https://jdmmedical.fr/">
+                <img src="https://jdmmedical.fr/img/logo.5f46bade.svg" alt="project" class=" mt-2 absolute bg-cover h-32 w-auto ">
+            </a>
+
+        </div>
+    </div>
 
     <div class="sm:w-1/3 w-full sm:mt-0 mt-16 relative px-4 h-full items-stretch justify-center flex">
         <div class="h-full  w-full shadow-3xl autre mx-auto justify-center items-center rounded-lg -p-4 hover:bg-black border border-gray-800">
             <img src="../../assets/img/index/chart.png" alt="project" class="flex bg-cover items-stretch h-48   w-auto">
-<!--            <a href="https://github.com/AlbertLanne/ProductionMonitoringMysql">-->
-<!--            <img src="../../assets/img/index/logo_project.jpg" alt="project" class="absolute bg-cover"> -->
-<!--            </a>-->
-
         </div>
         <div class= "absolute">
 
@@ -35,16 +32,34 @@
     </div>
 
 
-    <div class="sm:w-1/3 w-full sm:mt-0 mt-16
-                px-4 items-stretch justify-center flex">
+    <div class="sm:w-1/3 w-full sm:mt-0 mt-16 relative px-4 h-full items-stretch justify-center flex">
         <div class="h-full  w-full shadow-3xl autre mx-auto justify-center items-center rounded-lg -p-4 hover:bg-black border border-gray-800">
-            <a href="https://github.com/AlbertLanne/MEVN_herokuApp_mlab_boilerplate">
-                <img src="../../assets/img/index/Octocat.png" alt="project" class="my-6 h-32 w-auto mx-auto"> </a>
-            <div class="w-full flex">
-                <p class="text-white text-xl text-center flex mx-auto font-mono">MEVN stack project</p>
-            </div>
+            <img src="https://i.imgur.com/Gb6Bsl8.jpg" alt="project" class="h-48 w-full" style="object-fit: cover;">
+        </div>
+        <div class= "absolute">
+            <p class="text-yellow-800 px-2 bg-white rounded-lg text-xl text-center flex mx-auto font-mono mt-2 ">Residence France</p>
+            <a href="https://residencefrance.fr/" class="mx-auto justify-center w-full ">
+                <svg class="flex w-full mt-1" width="80px" height="auto" viewBox="0 0 2724 2019" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="Group" transform="translate(-194.000000, 75.000000)" stroke="#ED8936" stroke-width="150"><g id="Rectangle"><g><path d="M547.255781,782.797361 C550.48306,569.008448 610.0396,375.903672 704.711788,235.045698 C802.081536,90.1741613 1348.55783,0.569252787 1497.13906,0.569252787 L1573.79732,0.569252787 C1722.37855,0.569252787 2306.43037,90.1741613 2403.80012,235.045698 C2498.27225,375.60602 2557.77699,568.192568 2561.23491,781.442344 C2722.67771,840.494463 2842.45617,1057.82624 2842.45617,1316.89616 C2842.45617,1621.36504 2677.01996,1868.18583 2472.94405,1868.18583 L639.171212,1868.18583 C435.095303,1868.18583 269.659091,1621.36504 269.659091,1316.89616 C269.659091,1059.76882 387.648019,843.75604 547.255781,782.797361 Z M822.807605,922.369251 C1154.27734,992.433167 1404.68215,1025.72214 1574.02202,1022.23617 C1796.1213,1017.66412 2066.9531,990.584899 2263.11692,922.369251" id="Combined-Shape"></path></g></g></g></g></svg>
+            </a>
         </div>
     </div>
+
+<!--    <div class="sm:w-1/3 w-full sm:mt-0 mt-16 relative px-4 h-full items-stretch justify-center flex">-->
+<!--        <div class="h-full  w-full shadow-3xl autre mx-auto justify-center items-center rounded-lg -p-4 hover:bg-black border border-gray-800">-->
+<!--            <img src="https://jdmmedical.fr/img/specialiste-handicap-maintien-domicile-jdm-medical-xl.jpg" alt="project" class="h-48 w-full bg-right-bottom flex object-fill bg-cover">-->
+<!--        </div>-->
+<!--        <div class= "absolute">-->
+
+<!--            <p class="text-white text-xl text-center flex mx-auto font-mono mt-2">MEVN stack project</p>-->
+<!--            <a href="https://github.com/AlbertLanne/MEVN-stack-boilerplate">-->
+<!--                <img src="../../assets/img/index/Octocat.png" alt="project" class=" mt-2 absolute bg-cover h-32 w-auto ">-->
+<!--            </a>-->
+
+<!--        </div>-->
+<!--    </div>-->
+
+
+
 </div>
 <!--        <div class=" p-6 autre rounded-lg text-justify justify-around items-center  -mx-6  pb-8 w-1/3 ">-->
 <!--            <a href="https://github.com/AlbertLanne/ProductionMonitoringMysql" class="text-justify leading-normal text-copy-primary hover:text-copy-secondary">-->
--- pages/aboutWebsite.vue
+++ pages/aboutWebsite.vue
@@ -1,28 +1,27 @@
 <template>
-    <div>
+    <div class="w-full">
         <div class="text-justify mt-4 flex-col lg:flex-row h-full w-full flex items text-copy-primary bg-background-primary">
             <div class="mx-4 my-4  flex-wrap bg-background-secondary align-baseline items-center  hover:shadow-xl px-3 py-2 m-2 rounded ">
                 <p class="lg:text-2xl sm:text-3xl  text-2xl  my-4">The structure of the website</p>
 <!--SPA/VUEX/le créateur du logo/blender/ -->
                 <div class="mx-4 ">
-                    <div class=" mb-12 align-baseline flex sm:text-xl text-xl">
+                    <div class=" mb-6 align-baseline flex sm:text-xl text-xl">
                         <div class="my-auto rounded-lg justify-start w-full">
                             <div class="justify-end items-end flex">
                                 <p>This website is made with the javascript framework NuxtJS and Vue.JS and cooked
                                     by me with love. Nuxt.js is a great french framework for the server-side rendering!
-                                    I use too webpack and node.js for the environment of development and compilation.
+                                    I use too webpack and node.js for the environment of development.
                                     For the front end I use TailwindCSS. The version of the website are the <span
                                             class="mr-1"> v{{ $options.version }}</span>.
                                 </p>
                             </div>
                         </div>
 
-                        <div class="hidden lg:block
-                                    xl:mx-24 lg:mx-16 my-auto sm:mx-12 mx-6 flex">
+                        <div class="hidden lg:block xl:mx-12 lg:mx-8 my-auto sm:mx-4 mx-2 flex">
                             <picture>
                                 <img class="rounded-full bg-white border-solid "
-                                     height="260"
-                                     width="260"
+                                     height="200"
+                                     width="200"
                                      alt="My favorite french js framework ♥"
                                      src="../assets/img/AboutWebsite/nuxt.png">
                             </picture>
@@ -30,13 +29,12 @@
                     </div>
 
                     <div class="align-baseline flex">
-                        <div class="hidden lg:block
-                                    xl:mx-24 lg:mx-16 my-auto sm:mx-12 mx-6 flex">
+                        <div class="hidden lg:block xl:mx-12 lg:mx-8 my-auto sm:mx-4 mx-2 flex">
                             <picture>
                                 <img
                                         class="border-solid rounded-full shadow-inset hover:border-green-600"
-                                        height="260"
-                                        width="260"
+                                        height="200"
+                                        width="200"
                                         alt="The best Paas ever"
                                         src="../assets/img/AboutWebsite/logo_clever_cloud.jpg">
                             </picture>
@@ -44,72 +42,53 @@
                         <div class="sm:text-1xl text-xl my-auto rounded-lg justify-end w-full ">
                             <p>For the hosting and deployment, I use a French PAAS, Clever Cloud. Useful for easier
                                 deployment with git . The staff and the community are great. For the hosting of the
-                                domain name I use OVH. </p>
+                                domain name I use OVH. The website have also a local VueX store for a theme switcher and a
+                                middleware with MongoDB database </p>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div class="sm:w-3/5  md:w-auto lg:w-11/12 mx-4 my-4 bg-background-secondary align-baseline items-center hover:shadow-xl px-3 py-2 m-2 rounded ">
-                <p class="lg:text-2xl sm:text-3xl  text-2xl my-4">Data science</p>
+                <p class="lg:text-2xl sm:text-3xl  text-2xl my-4">Rest API</p>
 
-                <p class="lg:text-lg sm:text-2xl text-xl">The website has an important backend of
-                    data analysis. I time to watch the video of google SkillShop and have the certification on google
-                    analytics, google ads spend an important and general e-marketing. For the websites, I use different
-                    metrics tools for analyse the data of it and improve the experiences of the users. Mostly I watch
-                    the data from CleverCloud, Google Search Console and Google Analytics. You can see here more
-                    information about my <nuxt-link to="/privacy">privacy policy here.</nuxt-link>
+                <p class="sm:text-1xl text-xl">Like I have say the website work with a mongoDB
+                    connected with a Representational state transfer API creat by myself to improve the users
+                    experience and add like a online chat for my timeline page. Like/comments go on my database
+                    going throught my API. I have 2 others github project with the similar technologies.
+                    <a  href="https://github.com/AlbertLanne" class="italic border-b border-border-color-primary">See my github account!</a>
                 </p>
                 <!--                    <p class="text-3xl my-4">Google Ads</p>-->
             </div>
         </div>
 
+
+
                 <div class="mb-8 text-copy-primary hover:shadow-xl mt-4 mx-4 bg-background-secondary
-                              items-baseline flex hover:shadow-xl px-3 py-2 m-2 rounded ">
-                    <div id="app">
-                        <canvas id="myLineChart"></canvas>
+                              items-baseline  hover:shadow-xl px-3 py-2 m-2 rounded ">
+                    <div class="flex w-full">
+                        <div class="justify-start flex flex-wrap">
+                            <p class="lg:text-2xl sm:text-3xl  text-2xl my-4">Certification</p>
+                            <p class="sm:text-1xl text-xl my-2"> For the websites, I use different
+                                metrics tools for analyse the data of it and improve the users experiences. I have the Google
+                                certification of the Fundamental principles of digital marketing. And actually I'm working on
+                                more advanced google ads and google analytics certification. Mostly I watch
+                                the data from CleverCloud, Google Search Console and Google Analytics. You can see here more
+                                information about my <nuxt-link to="/privacy" class="border-b border-border-color-primary italic">privacy policy here.</nuxt-link>
+                            </p>
+                        </div>
                     </div>
-                    <div class="flex">
+
+                    <div class="w-full flex justify-center">
+                        <picture class="flex w-1/2 p-2">
+                            <img class="rounded-lg w-auto"  alt="My google certificat"
+                                 src="../assets/img/AboutWebsite/google_analytics.png"></picture>
+                        <picture class="flex w-1/2 p-2">
+                            <img class="rounded-lg w-auto" alt="My google certificat"
+                                 src="../assets/img/AboutWebsite/google_search_engine.png">
+                        </picture>
                     </div>
-                    </div>
-
-        <!--        <div class="mb-8 text-copy-primary hover:shadow-xl mt-4 mx-4 bg-background-secondary-->
-        <!--                      items-baseline flex hover:shadow-xl px-3 py-2 m-2 rounded ">-->
-        <!--            <div class="flex">-->
-
-
-        <!--                <div class="justify-start flex flex-wrap">-->
-        <!--                    <p class="lg:text-2xl sm:text-3xl  text-2xl my-4">Certification</p>-->
-        <!--                    <p class="sm:text-2xl text-xl">To understand and use well the data, I need to have certification. I-->
-        <!--                        have the-->
-        <!--                        Google-->
-        <!--                        certification of the Fundamental principles of digital marketing. And actually I'm working on-->
-        <!--                        more-->
-        <!--                        advanced-->
-        <!--                        google ads and google analytics certification.-->
-        <!--                    </p>-->
-        <!--                </div>-->
-        <!--            </div>-->
-        <!--        </div>-->
-
-        <!--                <div class="hidden xl:block justify-end flex flex-wrap">-->
-        <!--                    <picture class="flex w-2/4">-->
-        <!--                        <img class="rounded-lg w-auto"-->
-        <!--                             alt="My google certificat"-->
-        <!--                             src="../assets/img/AboutWebsite/google_analytics.png">-->
-        <!--                    </pi            </div>
-        cture>-->
-        <!--                </div>-->
-        <!--                <div class="hidden xl:block justify-end flex flex-wrap">-->
-        <!--                    <picture class="flex w-2/4">-->
-        <!--                        <img class="rounded-lg w-auto"-->
-        <!--                             alt="My google certificat"-->
-        <!--                             src="../assets/img/AboutWebsite/google_search_engine.png">-->
-        <!--                    </picture>-->
-        <!--                </div>-->
-
-
-        <!--                    <p class="text-3xl my-4">Google Ads</p>-->
+                </div>
     </div>
 </template>
 
