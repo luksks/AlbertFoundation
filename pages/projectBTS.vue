99026b6f1bd0a4d71721b3c50bd049f424b5d865
AlbertLanne <albert.lanne@gmail.com>
2020-04-10 21:18:20 +0200

    git

--- pages/projectBTS.vue
+++ pages/projectBTS.vue
@@ -1,84 +1,99 @@
 <template>
 
-<!--    <div>-->
-<!--        <div class=" justify-center justify-center flex bg-cover "-->
-<!--             style="background-image: url(https://i.imgur.com/Bx4yrrk.jpg)">-->
-<!--        <div class="flex flex-column col-8  w-full mx-8 justify-center my-8">-->
-<!--            <div class="flex flex-row items-end align-bottom">-->
-<!--                <div>-->
-<!--                <div class="ml-8">-->
-<!--                    <h2 class="text-xl"> Cooking Tools - Spatula </h2>-->
-<!--                    <h2 class="font-semibold text-3xl text-red-50  underline"> Silicone Cooking </h2>-->
-<!--                    <h3 class="font-bold "> Utensils Set </h3>-->
-<!--                    <button class="text-red-50 my-2 px-4 p-2 rounded-lg border-red-50"><p> BUY-->
-<!--                        NOW</p></button>-->
-<!--                </div>-->
-<!--                <div class="flex">-->
-<!--                <img class="border border-red-700 h-64 mr-4 mt-2"-->
-<!--                     src="https://ae01.alicdn.com/kf/HTB1e7VraEH1gK0jSZSyq6xtlpXaD/12PCS-Silicone-Kitchenware-Cooking-Utensils-Set-Heat-Resistant-Kitchen-Non-Stick-Cooking-Utensils-Baking-Tools-With.jpg">-->
-<!--                <img class="border border-red-700 h-64 mr-4 mt-2"-->
-<!--                     src="https://ae01.alicdn.com/kf/HTB1e7VraEH1gK0jSZSyq6xtlpXaD/12PCS-Silicone-Kitchenware-Cooking-Utensils-Set-Heat-Resistant-Kitchen-Non-Stick-Cooking-Utensils-Baking-Tools-With.jpg">-->
-<!--                <img class="border border-red-700 h-64 mr-5 mt-2"-->
-<!--                     src="https://ae01.alicdn.com/kf/HTB1e7VraEH1gK0jSZSyq6xtlpXaD/12PCS-Silicone-Kitchenware-Cooking-Utensils-Set-Heat-Resistant-Kitchen-Non-Stick-Cooking-Utensils-Baking-Tools-With.jpg">-->
-<!--                </div>-->
-<!--                </div>-->
-<!--               <div class="col-4 ">-->
-<!--                   <div class="pb-2/3 bg-red-900">-->
-<!--                   </div>-->
-<!--                    <img class=" border border-red-700 mr-4 mt-2  h-auto bg-red-200"-->
-<!--                         src="https://ae01.alicdn.com/kf/HTB1e7VraEH1gK0jSZSyq6xtlpXaD/12PCS-Silicone-Kitchenware-Cooking-Utensils-Set-Heat-Resistant-Kitchen-Non-Stick-Cooking-Utensils-Baking-Tools-With.jpg">-->
+    <div>
 
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--        </div>-->
+        <div class="flex w-full min-h-screen">
+            <div class="w-1/4 px-2 h-64"> <!-- sets width & gap -->
+                <div class="mt-6 rounded-lg bg-white text-center py-4"> <!-- card visuals into-->
+<h2>Kitchen Collection </h2>
+                </div>
 
+                <div class="rounded-t-lg my-2 bg-white  h-full w-full"> <!-- card visuals -->
+                    <!-- content -->
+                    <div class="items-stretch w-full bg-center bg-cover h-1/3 ">
+                        <img class="rounded-t-lg flex" src="https://ae01.alicdn.com/kf/H2ca1b298fc25480ebf1382bb0ba67ee7o/ATUCOHO-haute-couture-r-tro-vert-nordique-en-c-ramique-ensemble-de-vaisselle-ensemble-bol-assiette.jpg">
+                    </div>
+                    <div class="flex items-center justify-between px-4 py-2 bg-gray-400 rounded-b-lg">
+                        <h1 class="font-bold text-xl">Tableware</h1>
+                        <svg class="icon h-8 ml-16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200" height="200"><path d="M122.662 958.685L393.248 503.45 122.662 62.365 898.43 510.463 122.662 958.685zm0 0" fill="#2c2c2c"/></svg>                    </div>
+                </div>
 
-<!--&lt;!&ndash;        <div class="font-sans h-full w-full">&ndash;&gt;-->
-<!--&lt;!&ndash;            <div class="absolute h-auto w-full items-center align-baseline justify-center flex flex-wrap p-8 px-12 bg-cover"&ndash;&gt;-->
-<!--&lt;!&ndash;                 style="background-image: url(https://www.ot-saumur.fr/photo/art/grande/387832-30317936.jpg?v=1554892681)">&ndash;&gt;-->
-<!--&lt;!&ndash;                &lt;!&ndash;        <div class="absolute h-auto w-full items-center align-baseline justify-center bg-green-300 flex flex-wrap p-8 px-12 bg-cover" style="background: rgb(255,255,255);&ndash;&gt;&ndash;&gt;-->
-<!--&lt;!&ndash;                &lt;!&ndash;background: linear-gradient(169deg, rgba(255,255,255,1) 0%, rgba(219,51,50,1) 100%);">&ndash;&gt;&ndash;&gt;-->
-<!--&lt;!&ndash;                <div class="bg-fixed w-full justify-center flex">&ndash;&gt;-->
-<!--&lt;!&ndash;                    <div class=" flex">&ndash;&gt;-->
-<!--&lt;!&ndash;                        <div class="  w-1/2 justify-end text-end flex">&ndash;&gt;-->
-<!--&lt;!&ndash;                            <div class="justify-end">&ndash;&gt;-->
-<!--&lt;!&ndash;                                <h2 class="text-xl"> Cooking Tools - Spatula </h2>&ndash;&gt;-->
-<!--&lt;!&ndash;                                <h2 class="font-semibold text-3xl"> Silicone Cooking </h2>&ndash;&gt;-->
-<!--&lt;!&ndash;                                <h3 class="font-bold border-t-2 border-red-500"> Utensils Set </h3>&ndash;&gt;-->
-<!--&lt;!&ndash;                                <button class="bg-white text-red-50 my-2 px-4 p-2 rounded-lg border-red-50"><p> BUY&ndash;&gt;-->
-<!--&lt;!&ndash;                                    NOW</p></button>&ndash;&gt;-->
-<!--&lt;!&ndash;                            </div>&ndash;&gt;-->
-<!--&lt;!&ndash;                        </div>&ndash;&gt;-->
-<!--&lt;!&ndash;                        <div class="justify-end h-auto align-baseline flex items-end">&ndash;&gt;-->
-<!--&lt;!&ndash;                            <img class="border border-red-700 h-48 mr-4 mt-2"&ndash;&gt;-->
-<!--&lt;!&ndash;                                 src="https://ae01.alicdn.com/kf/HTB1e7VraEH1gK0jSZSyq6xtlpXaD/12PCS-Silicone-Kitchenware-Cooking-Utensils-Set-Heat-Resistant-Kitchen-Non-Stick-Cooking-Utensils-Baking-Tools-With.jpg">&ndash;&gt;-->
-<!--&lt;!&ndash;                            <img class="border border-red-700 h-48 mr-4 mt-2"&ndash;&gt;-->
-<!--&lt;!&ndash;                                 src="https://ae01.alicdn.com/kf/HTB1e7VraEH1gK0jSZSyq6xtlpXaD/12PCS-Silicone-Kitchenware-Cooking-Utensils-Set-Heat-Resistant-Kitchen-Non-Stick-Cooking-Utensils-Baking-Tools-With.jpg">&ndash;&gt;-->
-<!--&lt;!&ndash;                            <img class="border border-red-700 h-48 mr-5 mt-2"&ndash;&gt;-->
-<!--&lt;!&ndash;                                 src="https://ae01.alicdn.com/kf/HTB1e7VraEH1gK0jSZSyq6xtlpXaD/12PCS-Silicone-Kitchenware-Cooking-Utensils-Set-Heat-Resistant-Kitchen-Non-Stick-Cooking-Utensils-Baking-Tools-With.jpg">&ndash;&gt;-->
-<!--&lt;!&ndash;                            <img class="border border-red-700 bg-cover " style="width: 400px; height: auto"&ndash;&gt;-->
-<!--&lt;!&ndash;                                 src="https://ae01.alicdn.com/kf/HTB1e7VraEH1gK0jSZSyq6xtlpXaD/12PCS-Silicone-Kitchenware-Cooking-Utensils-Set-Heat-Resistant-Kitchen-Non-Stick-Cooking-Utensils-Baking-Tools-With.jpg">&ndash;&gt;-->
-<!--&lt;!&ndash;                        </div>&ndash;&gt;-->
-<!--&lt;!&ndash;                    </div>&ndash;&gt;-->
-<!--&lt;!&ndash;                </div>&ndash;&gt;-->
-<!--&lt;!&ndash;            </div>&ndash;&gt;-->
+            </div>
 
-<!--&lt;!&ndash;        </div>&ndash;&gt;-->
+            <div class="w-1/4 px-2 h-64"> <!-- sets width & gap -->
+                <div class="rounded-t-lg my-2 bg-white  h-full w-full"> <!-- card visuals -->
+                    <!-- content -->
+                    <div class="items-stretch w-full bg-center bg-cover h-1/3 ">
+                        <img class="rounded-t-lg flex" src="https://ae01.alicdn.com/kf/H2ca1b298fc25480ebf1382bb0ba67ee7o/ATUCOHO-haute-couture-r-tro-vert-nordique-en-c-ramique-ensemble-de-vaisselle-ensemble-bol-assiette.jpg">
+                    </div>
+                    <div class="flex items-center justify-between px-4 py-2 bg-gray-400 rounded-b-lg">
+                        <h1 class="font-bold text-xl">Tableware</h1>
+                        <svg class="icon h-8 ml-16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200" height="200"><path d="M122.662 958.685L393.248 503.45 122.662 62.365 898.43 510.463 122.662 958.685zm0 0" fill="#2c2c2c"/></svg>                    </div>
+                </div>
+
+            </div>
+
+            <div class="w-1/4 px-2 h-64"> <!-- sets width & gap -->
+                <div class="rounded-t-lg my-2 bg-white  h-full w-full"> <!-- card visuals -->
+                    <!-- content -->
+                    <div class="items-stretch w-full bg-center bg-cover h-1/3 ">
+                        <img class="rounded-t-lg flex" src="https://ae01.alicdn.com/kf/H2ca1b298fc25480ebf1382bb0ba67ee7o/ATUCOHO-haute-couture-r-tro-vert-nordique-en-c-ramique-ensemble-de-vaisselle-ensemble-bol-assiette.jpg">
+                    </div>
+                    <div class="flex items-center justify-between px-4 py-2 bg-gray-400 rounded-b-lg">
+                        <h1 class="font-bold text-xl">Tableware</h1>
+                        <svg class="icon h-8 ml-16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200" height="200"><path d="M122.662 958.685L393.248 503.45 122.662 62.365 898.43 510.463 122.662 958.685zm0 0" fill="#2c2c2c"/></svg>                    </div>
+                </div>
+
+            </div>
+            <div class="w-1/4 px-2 h-64"> <!-- sets width & gap -->
+                <div class="rounded-t-lg my-2 bg-white  h-full w-full"> <!-- card visuals -->
+                    <!-- content -->
+                    <div class="items-stretch w-full bg-center bg-cover h-1/3 ">
+                        <img class="rounded-t-lg flex" src="https://ae01.alicdn.com/kf/H2ca1b298fc25480ebf1382bb0ba67ee7o/ATUCOHO-haute-couture-r-tro-vert-nordique-en-c-ramique-ensemble-de-vaisselle-ensemble-bol-assiette.jpg">
+                    </div>
+                    <div class="flex items-center justify-between px-4 py-2 bg-gray-400 rounded-b-lg">
+                        <h1 class="font-bold text-xl">Tableware</h1>
+                        <svg class="icon h-8 ml-16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200" height="200"><path d="M122.662 958.685L393.248 503.45 122.662 62.365 898.43 510.463 122.662 958.685zm0 0" fill="#2c2c2c"/></svg>                    </div>
+                </div>
+
+            </div>
+        </div>
 
 
-<!--    </div>-->
+        <!--        <div class=" justify-center justify-center flex bg-cover "-->
+        <!--             style="background-image: url(https://i.imgur.com/Bx4yrrk.jpg)">-->
+        <!--        <div class="flex flex-column col-8  w-full mx-8 justify-center my-8">-->
+        <!--            <div class="flex flex-row items-end align-bottom">-->
+        <!--                <div>-->
+        <!--                <div class="ml-8">-->
+        <!--                    <h2 class="text-xl"> Cooking Tools - Spatula </h2>-->
+        <!--                    <h2 class="font-semibold text-3xl text-red-50  underline"> Silicone Cooking </h2>-->
+        <!--                    <h3 class="font-bold "> Utensils Set </h3>-->
+        <!--                    <button class="text-red-50 my-2 px-4 p-2 rounded-lg border-red-50"><p> BUY-->
+        <!--                        NOW</p></button>-->
+        <!--                </div>-->
+        <!--                <div class="flex">-->
+        <!--                <img class="border border-red-700 h-64 mr-4 mt-2"-->
+        <!--                     src="https://ae01.alicdn.com/kf/HTB1e7VraEH1gK0jSZSyq6xtlpXaD/12PCS-Silicone-Kitchenware-Cooking-Utensils-Set-Heat-Resistant-Kitchen-Non-Stick-Cooking-Utensils-Baking-Tools-With.jpg">-->
+        <!--                <img class="border border-red-700 h-64 mr-4 mt-2"-->
+        <!--                     src="https://ae01.alicdn.com/kf/HTB1e7VraEH1gK0jSZSyq6xtlpXaD/12PCS-Silicone-Kitchenware-Cooking-Utensils-Set-Heat-Resistant-Kitchen-Non-Stick-Cooking-Utensils-Baking-Tools-With.jpg">-->
+        <!--                <img class="border border-red-700 h-64 mr-5 mt-2"-->
+        <!--                     src="https://ae01.alicdn.com/kf/HTB1e7VraEH1gK0jSZSyq6xtlpXaD/12PCS-Silicone-Kitchenware-Cooking-Utensils-Set-Heat-Resistant-Kitchen-Non-Stick-Cooking-Utensils-Baking-Tools-With.jpg">-->
+        <!--                </div>-->
+        <!--                </div>-->
+        <!--               <div class="col-4 ">-->
+        <!--                   <div class="pb-2/3 bg-red-900">-->
+        <!--                   </div>-->
+        <!--                    <img class=" border border-red-700 mr-4 mt-2  h-auto bg-red-200"-->
+        <!--                         src="https://ae01.alicdn.com/kf/HTB1e7VraEH1gK0jSZSyq6xtlpXaD/12PCS-Silicone-Kitchenware-Cooking-Utensils-Set-Heat-Resistant-Kitchen-Non-Stick-Cooking-Utensils-Baking-Tools-With.jpg">-->
+
+        <!--                </div>-->
+        <!--            </div>-->
+        <!--        </div>-->
+        <!--        </div>-->
 
 
-
-
-
-
-
-
-
-
-
+    </div>
 
 
 </template>
