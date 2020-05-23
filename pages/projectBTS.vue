a058eda8af3265f0d673cca07f18f5e05c89a83f
AlbertLanne <albert.lanne@gmail.com>
2020-05-23 18:51:53 +0200

    update

--- pages/projectBTS.vue
+++ pages/projectBTS.vue
@@ -1,88 +1,130 @@
 <template>
+    <div>
 
-    <div class="page-width bg-orange-200">
 
-        <div class="flex w-full min-h-screen">
-            <div class="w-1/4 px-2 h-64"> <!-- sets width & gap -->
-                <div class="mt-6 rounded-lg bg-white text-center py-4"> <!-- card visuals into-->
-                    <h2>Kitchen Collection </h2>
+        <div class="w-full justify-center  text-center mx-auto">
+            <h2>HOW IT WORKS</h2>
+            <div class="justify-center  text-center mx-auto">
+                <div class="w-1/2 justify-center mx-auto">
+   <img src="https://i.imgur.com/AVL1VjX.png">
+   <img src="https://i.imgur.com/xcX51Gk.png">
+   <img src="https://i.imgur.com/fMbSoR4.png">
+   <img src="https://i.imgur.com/43p1V9D.png">
+   <img src="https://i.imgur.com/mmMAG3s.png">
                 </div>
-
-                <div class="rounded-t-lg my-2 bg-white  h-full w-full"> <!-- card visuals -->
-                    <!-- content -->
-                    <div class="items-stretch w-full bg-center bg-cover h-1/3 ">
-                        <img class="rounded-t-lg flex"
-                             src="https://ae01.alicdn.com/kf/H2ca1b298fc25480ebf1382bb0ba67ee7o/ATUCOHO-haute-couture-r-tro-vert-nordique-en-c-ramique-ensemble-de-vaisselle-ensemble-bol-assiette.jpg">
-                    </div>
-                    <div class="flex items-center justify-between px-4 py-2 bg-gray-400 rounded-b-lg">
-                        <h1 class="font-bold text-xl">Tableware</h1>
-                        <svg class="icon h-8 ml-16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200"
-                             height="200">
-                            <path d="M122.662 958.685L393.248 503.45 122.662 62.365 898.43 510.463 122.662 958.685zm0 0"
-                                  fill="#2c2c2c"/>
-                        </svg>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="w-1/4 px-2 h-64"> <!-- sets width & gap -->
-                <div class="rounded-t-lg my-2 bg-white  h-full w-full"> <!-- card visuals -->
-                    <!-- content -->
-                    <div class="items-stretch w-full bg-center bg-cover h-1/3 ">
-                        <img class="rounded-t-lg flex"
-                             src="https://ae01.alicdn.com/kf/H2ca1b298fc25480ebf1382bb0ba67ee7o/ATUCOHO-haute-couture-r-tro-vert-nordique-en-c-ramique-ensemble-de-vaisselle-ensemble-bol-assiette.jpg">
-                    </div>
-                    <div class="flex items-center justify-between px-4 py-2 bg-gray-400 rounded-b-lg">
-                        <h1 class="font-bold text-xl">Tableware</h1>
-                        <svg class="icon h-8 ml-16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200"
-                             height="200">
-                            <path d="M122.662 958.685L393.248 503.45 122.662 62.365 898.43 510.463 122.662 958.685zm0 0"
-                                  fill="#2c2c2c"/>
-                        </svg>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="w-1/4 px-2 h-64"> <!-- sets width & gap -->
-                <div class="rounded-t-lg my-2 bg-white  h-full w-full"> <!-- card visuals -->
-                    <!-- content -->
-                    <div class="items-stretch w-full bg-center bg-cover h-1/3 ">
-                        <img class="rounded-t-lg flex"
-                             src="https://ae01.alicdn.com/kf/H2ca1b298fc25480ebf1382bb0ba67ee7o/ATUCOHO-haute-couture-r-tro-vert-nordique-en-c-ramique-ensemble-de-vaisselle-ensemble-bol-assiette.jpg">
-                    </div>
-                    <div class="flex items-center justify-between px-4 py-2 bg-gray-400 rounded-b-lg">
-                        <h1 class="font-bold text-xl">Tableware</h1>
-                        <svg class="icon h-8 ml-16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200"
-                             height="200">
-                            <path d="M122.662 958.685L393.248 503.45 122.662 62.365 898.43 510.463 122.662 958.685zm0 0"
-                                  fill="#2c2c2c"/>
-                        </svg>
-                    </div>
-                </div>
-
-            </div>
-            <div class="w-1/4 px-2 h-64"> <!-- sets width & gap -->
-                <div class="rounded-t-lg my-2 bg-white  h-full w-full"> <!-- card visuals -->
-                    <!-- content -->
-                    <div class="items-stretch w-full bg-center bg-cover h-1/3 ">
-                        <img class="rounded-t-lg flex"
-                             src="https://ae01.alicdn.com/kf/H2ca1b298fc25480ebf1382bb0ba67ee7o/ATUCOHO-haute-couture-r-tro-vert-nordique-en-c-ramique-ensemble-de-vaisselle-ensemble-bol-assiette.jpg">
-                    </div>
-                    <div class="flex items-center justify-between px-4 py-2 bg-gray-400 rounded-b-lg">
-                        <h1 class="font-bold text-xl">Tableware</h1>
-                        <svg class="icon h-8 ml-16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200"
-                             height="200">
-                            <path d="M122.662 958.685L393.248 503.45 122.662 62.365 898.43 510.463 122.662 958.685zm0 0"
-                                  fill="#2c2c2c"/>
-                        </svg>
-                    </div>
-                </div>
-
-            </div>
         </div>
+        </div>
+        <div class="w-full justify-center  text-center mx-auto">
+
+
+            <h2>HOW MANY DO I NEED</h2>
+            <p> For homes that often have mosquitoes inside, we recommend purchasing 2 or more,a unit for each living
+                room and bedroom to maximize the area of protection for a typical household. If you infrequently have
+                mosquitoes in the house, we recommend starting with a unit near each main entrance to the home.
+            </p>
+            <h2> SHIPPING & DELIVERY</h2>
+            <p>
+                All orders are expertly packaged, prepared, and shipped within one business day of ordering. We offer
+                FREE shipping worldwide and the typical delivery time frame is between 12-22 business days. Depending on
+                your location, you may receive items much earlier.
+                All orders are shipped with the tracking number so you can track it every step of the way! Occasionally,
+                packages may be faced with delays beyond our control such as customs or postal delays.
+            </p>
+            <h2>30 DAY RISK-FREE GUARANTEE</h2>
+            <p>
+                We have an iron-clad risk-free 30-day guarantee that begins only when you receive the product!
+                If you don't have a positive experience for any reason, we will do whatever it takes to make sure you
+                are 100% satisfied with your purchase.
+            </p>
+
+        </div>
+
     </div>
+
+
+    <!--    <div class="page-width bg-orange-200">-->
+
+    <!--        <div class="flex w-full min-h-screen">-->
+    <!--            <div class="w-1/4 px-2 h-64"> &lt;!&ndash; sets width & gap &ndash;&gt;-->
+    <!--                <div class="mt-6 rounded-lg bg-white text-center py-4"> &lt;!&ndash; card visuals into&ndash;&gt;-->
+    <!--                    <h2>Kitchen Collection </h2>-->
+    <!--                </div>-->
+
+    <!--                <div class="rounded-t-lg my-2 bg-white  h-full w-full"> &lt;!&ndash; card visuals &ndash;&gt;-->
+    <!--                    &lt;!&ndash; content &ndash;&gt;-->
+    <!--                    <div class="items-stretch w-full bg-center bg-cover h-1/3 ">-->
+    <!--                        <img class="rounded-t-lg flex"-->
+    <!--                             src="https://ae01.alicdn.com/kf/H2ca1b298fc25480ebf1382bb0ba67ee7o/ATUCOHO-haute-couture-r-tro-vert-nordique-en-c-ramique-ensemble-de-vaisselle-ensemble-bol-assiette.jpg">-->
+    <!--                    </div>-->
+    <!--                    <div class="flex items-center justify-between px-4 py-2 bg-gray-400 rounded-b-lg">-->
+    <!--                        <h1 class="font-bold text-xl">Tableware</h1>-->
+    <!--                        <svg class="icon h-8 ml-16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200"-->
+    <!--                             height="200">-->
+    <!--                            <path d="M122.662 958.685L393.248 503.45 122.662 62.365 898.43 510.463 122.662 958.685zm0 0"-->
+    <!--                                  fill="#2c2c2c"/>-->
+    <!--                        </svg>-->
+    <!--                    </div>-->
+    <!--                </div>-->
+
+    <!--            </div>-->
+
+    <!--            <div class="w-1/4 px-2 h-64"> &lt;!&ndash; sets width & gap &ndash;&gt;-->
+    <!--                <div class="rounded-t-lg my-2 bg-white  h-full w-full"> &lt;!&ndash; card visuals &ndash;&gt;-->
+    <!--                    &lt;!&ndash; content &ndash;&gt;-->
+    <!--                    <div class="items-stretch w-full bg-center bg-cover h-1/3 ">-->
+    <!--                        <img class="rounded-t-lg flex"-->
+    <!--                             src="https://ae01.alicdn.com/kf/H2ca1b298fc25480ebf1382bb0ba67ee7o/ATUCOHO-haute-couture-r-tro-vert-nordique-en-c-ramique-ensemble-de-vaisselle-ensemble-bol-assiette.jpg">-->
+    <!--                    </div>-->
+    <!--                    <div class="flex items-center justify-between px-4 py-2 bg-gray-400 rounded-b-lg">-->
+    <!--                        <h1 class="font-bold text-xl">Tableware</h1>-->
+    <!--                        <svg class="icon h-8 ml-16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200"-->
+    <!--                             height="200">-->
+    <!--                            <path d="M122.662 958.685L393.248 503.45 122.662 62.365 898.43 510.463 122.662 958.685zm0 0"-->
+    <!--                                  fill="#2c2c2c"/>-->
+    <!--                        </svg>-->
+    <!--                    </div>-->
+    <!--                </div>-->
+
+    <!--            </div>-->
+
+    <!--            <div class="w-1/4 px-2 h-64"> &lt;!&ndash; sets width & gap &ndash;&gt;-->
+    <!--                <div class="rounded-t-lg my-2 bg-white  h-full w-full"> &lt;!&ndash; card visuals &ndash;&gt;-->
+    <!--                    &lt;!&ndash; content &ndash;&gt;-->
+    <!--                    <div class="items-stretch w-full bg-center bg-cover h-1/3 ">-->
+    <!--                        <img class="rounded-t-lg flex"-->
+    <!--                             src="https://ae01.alicdn.com/kf/H2ca1b298fc25480ebf1382bb0ba67ee7o/ATUCOHO-haute-couture-r-tro-vert-nordique-en-c-ramique-ensemble-de-vaisselle-ensemble-bol-assiette.jpg">-->
+    <!--                    </div>-->
+    <!--                    <div class="flex items-center justify-between px-4 py-2 bg-gray-400 rounded-b-lg">-->
+    <!--                        <h1 class="font-bold text-xl">Tableware</h1>-->
+    <!--                        <svg class="icon h-8 ml-16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200"-->
+    <!--                             height="200">-->
+    <!--                            <path d="M122.662 958.685L393.248 503.45 122.662 62.365 898.43 510.463 122.662 958.685zm0 0"-->
+    <!--                                  fill="#2c2c2c"/>-->
+    <!--                        </svg>-->
+    <!--                    </div>-->
+    <!--                </div>-->
+
+    <!--            </div>-->
+    <!--            <div class="w-1/4 px-2 h-64"> &lt;!&ndash; sets width & gap &ndash;&gt;-->
+    <!--                <div class="rounded-t-lg my-2 bg-white  h-full w-full"> &lt;!&ndash; card visuals &ndash;&gt;-->
+    <!--                    &lt;!&ndash; content &ndash;&gt;-->
+    <!--                    <div class="items-stretch w-full bg-center bg-cover h-1/3 ">-->
+    <!--                        <img class="rounded-t-lg flex"-->
+    <!--                             src="https://ae01.alicdn.com/kf/H2ca1b298fc25480ebf1382bb0ba67ee7o/ATUCOHO-haute-couture-r-tro-vert-nordique-en-c-ramique-ensemble-de-vaisselle-ensemble-bol-assiette.jpg">-->
+    <!--                    </div>-->
+    <!--                    <div class="flex items-center justify-between px-4 py-2 bg-gray-400 rounded-b-lg">-->
+    <!--                        <h1 class="font-bold text-xl">Tableware</h1>-->
+    <!--                        <svg class="icon h-8 ml-16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200"-->
+    <!--                             height="200">-->
+    <!--                            <path d="M122.662 958.685L393.248 503.45 122.662 62.365 898.43 510.463 122.662 958.685zm0 0"-->
+    <!--                                  fill="#2c2c2c"/>-->
+    <!--                        </svg>-->
+    <!--                    </div>-->
+    <!--                </div>-->
+
+    <!--            </div>-->
+    <!--        </div>-->
+    <!--    </div>-->
 
 
 </template>
